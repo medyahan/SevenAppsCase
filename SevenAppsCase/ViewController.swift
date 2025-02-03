0d042a59aa9da04d477f8d8af45dd1cfa1e92c0e
Medya <33479784+medyahan@users.noreply.github.com>
2025-02-03 12:20:32 +0300

    Project Uploaded

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by Medya Han on 3.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
